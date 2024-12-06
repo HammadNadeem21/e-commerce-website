42aa73ab82ab7dce275a0900aa1b4b3dd52515a2
HammadNadeem21 <nhammad424@gmail.com>
2024-12-06 14:37:32 +0500

    updated

---
+++ e-commerce-website/src/components/TopHeader.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+// Font
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+    weight:"500",
+
+    display:"swap",
+    subsets: ['latin']
+})
+
+const TopHeader = () => {
+  return (
+    <div className='w-full h-[48px] bg-black py-[12px]'>
+
+      <div className='w-[859px] h-[24px] ml-[445px]'>
+
+<div className={`${poppins.className} w-[550px] h-[24] flex items-center gap-[10px]`}>
+    <h1 className='text-[#D9D9D9] text-[14px] font-normal whitespace-nowrap'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</h1>
+    <button className='underline w-[68px] text-[#FAFAFA] font-semibold text-[14px]'>ShopNow</button>
+</div>
+
+<div className='w-[78px] h-[24px]'>
+</div>
+
+      </div>
+    </div>
+  )
+}
+
+export default TopHeader
